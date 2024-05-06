a0397bf77e8832ae08f82000f8581676e711d7d0
soljm <vee.bwii27@gmail.com>
2024-05-06 14:14:11 +1000

    minor editing of comments and code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-
 #################################################
 # Database Setup
 #################################################
@@ -30,6 +29,7 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# Homepage route
 @app.route("/")
 def homepage():
     return """
@@ -42,99 +42,126 @@ def homepage():
     /api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>
 """
 
+# Create function that returns date of one year ago
 def one_year_ago(session):
     return session.query(func.max(func.date(func.julianday(Measurement.date, '-1 year')))).scalar_subquery()
 
-def most_active(session):
-    return session.query(Measurement.station)\
-    .group_by(Measurement.station).order_by(func.count(Measurement.station).desc())\
-    .limit(1).scalar_subquery()
-
+# Precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create a session
     session = Session(engine)
 
+    # Query precipitation data within the last 12 months
     prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago(session)).all()
 
+    # Close session
     session.close()
 
-    # Create dictionary
+    # Create dictionary from precipitation data
     prcp_dict = {}
     for date, prcp in prcp_data:
         prcp_dict[date] = prcp
 
+    # Return dictionary (already in JSON)
     return prcp_dict
 
+# Stations route
 @app.route("/api/v1.0/stations")
 def stations():
     # Create session
     session = Session(engine)
 
+    # Query station list
     station_results = session.query(Station.station).all()
 
+    # Close session
     session.close()
 
-    # Create list
+    # Create list of station IDs
     station_list = []
     for station, in station_results:
         station_list.append(station)
 
+    # Return list (already in JSON)
     return station_list
 
+# Temperature route
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Create session
     session = Session(engine)
 
-    temp = session.query(Measurement.date, Measurement.tobs)\
-    .filter(Measurement.date >= one_year_ago(session)).filter(Measurement.station == most_active(session)).all()
+    # Subquery to find most active station ID
+    most_active = session.query(Measurement.station)\
+    .group_by(Measurement.station).order_by(func.count(Measurement.station).desc())\
+    .limit(1).scalar_subquery()
 
+    # Query for temperature of most active station within 12 months
+    temp = session.query(Measurement.date, Measurement.tobs)\
+    .filter(Measurement.date >= one_year_ago(session)).filter(Measurement.station == most_active).all()
+
+    # Close session
     session.close()
 
-    # Create list
+    # Create list of temperature data
     tobs_list = []
     for date, tobs in temp:
         tobs_dict = {
-            "date": date,
-            "tobs": tobs
+            "Date": date,
+            "Temperature": tobs
         }
         tobs_list.append(tobs_dict)
     
+    # Return list of temperature (already in JSON)
     return tobs_list
 
+# Dynamic start date route
 @app.route("/api/v1.0/<start>")
 def start_date(start):
+    # Create session
     session = Session(engine)
 
+    # Query min, max, and average of user inputed start date 
     tmin, tmax, tavg = session.query(
             func.min(Measurement.tobs),
             func.max(Measurement.tobs),
             func.avg(Measurement.tobs)
         ).filter(Measurement.date >= start).first()
 
+    # Close session
     session.close()
+
+    # Return min, average, and max (alraedy in JSON)
     return {
         "TMIN": tmin,
         "TAVG": tavg,
         "TMAX": tmax
     }
 
+# Dynamic start and end date route
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start,end):
+    # Create session
     session = Session(engine)
 
+    # Query min, max, and average of temperature between dates (inclusive) 
     tmin, tmax, tavg = session.query(
             func.min(Measurement.tobs),
             func.max(Measurement.tobs),
             func.avg(Measurement.tobs)
         ).filter(Measurement.date >= start).filter(Measurement.date <= end).first()
 
+    # Close session
     session.close()
+
+    # Return min, average, and max (already in JSON)
     return {
         "TMIN": tmin,
         "TAVG": tavg,
         "TMAX": tmax
     }
 
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
